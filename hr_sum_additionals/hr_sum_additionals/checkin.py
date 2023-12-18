9b6d4d4ecbf0e342566d556490912af1561232c1
fr3ooon1 <mohamed.essam68.me@gmail.com>
2023-12-18 10:12:20 +0200

    Update V1.1.3

--- hr_sum_additionals/hr_sum_additionals/checkin.py
+++ hr_sum_additionals/hr_sum_additionals/checkin.py
@@ -13,18 +13,30 @@ class CustomCheckin(EmployeeCheckin):
         date = getdate(datatime)
         time_checkIN = get_time(datatime)
         name = self.name
-        log_type = self.log_type
+        # log_type = self.log_type
         shift = self.shift
 
 
         shift_data = frappe.get_doc("Shift Type" , shift)
-        to_time = datetime.strptime(str(shift_data.late_penalty_after), "%H:%M:%S")
-        from_time = datetime.strptime(str(time_checkIN), "%H:%M:%S")
-
-        log_type_condition = "Log_type == "+log_type 
-        shift_condition = "shift == "+shift
-        condition = log_type_condition + " AND " +shift_condition
+        datetime_obj = datetime.strptime(str(shift_data.late_penalty_after ), "%H:%M:%S")
+        to_time = datetime_obj.time()
+        
+        from_time = time_checkIN
 
 
         self.deduction = get_the_rule (employee , date , doctype , to_time , from_time , name )
+
+    
+# @frappe.whitelist()
+# def dates ():
+#     shift_data = frappe.get_doc("Shift Type" , "fr3on shift")
+#     datetime_obj = datetime.strptime(str(shift_data.late_penalty_after ), "%H:%M:%S")
+#     date = datetime_obj.date()
+#     time = datetime_obj.time()
+
+#     # Do something with date and time if needed
+#     print("Date:", date)
+#     print("Time:", time)
+   
+
 
